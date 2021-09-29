eb505cd1cdb336fe667cd722e6175a0836a8ddaf
Moshfegh Hamedani <moshfegh@live.com.au>
2021-09-29 18:37:23 -0400

    .

--- storefront/settings/prod.py
+++ storefront/settings/prod.py
@@ -1,4 +1,3 @@
-from storefront.settings.dev import SECRET_KEY
 from .common import *
 import os
 import dj_database_url
@@ -9,7 +8,7 @@ ALLOWED_HOSTS = [
     'enigmatic-everglades-98656.herokuapp.com'
 ]
 
-SECRET_KEY = os.environ('SECRET_KEY')
+SECRET_KEY = os.environ['SECRET_KEY']
 
 DATABASE = {
     'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
