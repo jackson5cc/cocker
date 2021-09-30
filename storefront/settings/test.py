39495f64cf47dd7e70a89f709dfa3036ee41ed1b
Moshfegh Hamedani <moshfegh@live.com.au>
2021-09-30 16:44:30 -0400

    .

---
+++ storefront/settings/test.py
@@ -0,0 +1,15 @@
+from .common import *
+import os
+import dj_database_url
+
+DEBUG = False
+
+ALLOWED_HOSTS = [
+    'cocker-test.herokuapp.com'
+]
+
+SECRET_KEY = os.environ['SECRET_KEY']
+
+DATABASES = {
+    'default':dj_database_url.config(conn_max_age=600)
+}
