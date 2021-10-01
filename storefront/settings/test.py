3eb76b87327dfafd3baa01fdb6341f37aeda58de
Moshfegh Hamedani <moshfegh@live.com.au>
2021-10-01 12:37:07 -0400

    x

--- storefront/settings/test.py
+++ storefront/settings/test.py
@@ -10,13 +10,15 @@ ALLOWED_HOSTS = [
 
 SECRET_KEY = os.environ['SECRET_KEY']
 
-DATABASES = {
-    'default':dj_database_url.config(conn_max_age=600)
-}
+# DATABASES = {
+#     'default':dj_database_url.config(conn_max_age=600)
+# }
 
+import os
 from pprint import pprint
 print('MOSH DEBUG')
-print(DATABASES)
+print(os.environ['DATABASE_URL'])
+# print(DATABASES)
 
 # DATABASES['default']['TEST'] = { 
 #   'NAME': DATABASES['default']['NAME']
