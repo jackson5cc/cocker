e6221b7f114dbf9e6fac27f90912dcf7271bccdd
Moshfegh Hamedani <moshfegh@live.com.au>
2021-10-01 10:05:40 -0400

    set test db name

--- storefront/settings/test.py
+++ storefront/settings/test.py
@@ -13,3 +13,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 DATABASES = {
     'default':dj_database_url.config(conn_max_age=600)
 }
+
+DATABASES['default']['TEST'] = { 
+  'NAME': DATABASES['default']['NAME']
+}
