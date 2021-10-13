05fa60510ba2cf02f6c016267d7a9d50d0cc3ae2
Moshfegh Hamedani <moshfegh@live.com.au>
2021-10-13 19:38:38 -0400

    Working with images

--- playground/views.py
+++ playground/views.py
@@ -1,5 +1,25 @@
-from django.shortcuts import render
+from django.db.models.fields.files import ImageFieldFile
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from store.models import ProductImage
 
 
+@api_view()
 def say_hello(request):
-    return render(request, 'hello.html', {'name': 'Mosh'})
+    image = ProductImage.objects.get(pk=19)
+    photo: ImageFieldFile = image.photo 
+
+    # methods inherited from File 
+    photo.open()
+    photo.save()
+    photo.delete()
+
+    return Response({
+      'name': photo.name,
+      # 'path': photo.path,
+      'url': photo.url,
+      'size': photo.size,
+      'height': photo.height,
+      'width': photo.width
+    })
