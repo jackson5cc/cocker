cc7c9498c8655fc3373ee8a2fea345ccc1cf5591
Moshfegh Hamedani <moshfegh@live.com.au>
2021-10-13 20:55:53 -0400

    Managing static assets

--- storefront/urls.py
+++ storefront/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 import debug_toolbar
 
@@ -30,5 +31,6 @@ urlpatterns = [
     path('store/', include('store.urls')),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
+    path('', TemplateView.as_view(template_name='index.html')),
     path('__debug__/', include(debug_toolbar.urls)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
